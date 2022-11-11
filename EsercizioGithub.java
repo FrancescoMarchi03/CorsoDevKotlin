10e89ace1010981914dca947aff454a22b0638d4
FrancescoMarchi03 <117976606+FrancescoMarchi03@users.noreply.github.com>
2022-11-11 11:31:00 +0100

    Nuovo file Java
    Aggiunto il file java con l'esercizio richiesto

---
+++ EsercizioGithub.java
@@ -0,0 +1,18 @@
+public class EsercizioGithub {
+    //creazione variabili con 
+    double charb = "b";
+    double numf = 123.34;
+    double numi = 1;
+    double cont;
+    double ris;
+    bool b1=True;
+    bool b2=False;
+    cont = charb + numf + numi;
+    ris = cont/3;
+    System.out.println(charb);
+    System.out.println(numf);
+    System.out.println(numi);
+    System.out.prinln(ris);
+    System.out.println("Evviva");
+    b1 = True;
+}
